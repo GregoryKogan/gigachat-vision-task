dacf54c55bf60c925b608bbabdab5a52828e47c3
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2025-11-26 19:54:52 +0300

    refactor: stream dataset in generate_input.py without loading it into memory

--- generate_input.py
+++ generate_input.py
@@ -1,7 +1,5 @@
-import datasets
-import pandas as pd
 import argparse
-import csv
+from datasets import load_dataset
 
 
 DEFAULT_DATASET_NAME = "google-research-datasets/conceptual_captions"
@@ -9,46 +7,37 @@ DEFAULT_SPLIT = "train"
 DEFAULT_IMG_URL_COLUMN = "image_url"
 
 
-def main():
+def main() -> None:
     args = parse_args()
-    img_urls = load_img_urls(args.dataset_name, args.split, args.img_url_column)
-    random_img_urls = select_random_img_urls(img_urls, args.n)
-    save_img_urls(random_img_urls, args.output_path)
+
+    dataset = load_dataset(args.dataset_name, split=args.split, streaming=True)
+    dataset = dataset.select_columns([args.img_url_column])
+    dataset = dataset.rename_column(args.img_url_column, "img_url")
+    dataset = dataset.take(args.n)
+    dataset.to_csv(args.output_path, index=False)
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Generate input image URLs")
     parser.add_argument(
-        "--n", type=int, required=True, help="Number of random image URLs to save"
+        "--n", type=int, required=True, help="Number of URLs to collect"
     )
     parser.add_argument(
-        "--output-path", type=str, required=True, help="Output txt file path"
+        "--output-path", type=str, required=True, help="Path to output CSV file"
     )
     parser.add_argument(
-        "--dataset-name", type=str, default=DEFAULT_DATASET_NAME, help="Dataset name"
+        "--dataset-name", type=str, default=DEFAULT_DATASET_NAME, help="HF Dataset name"
     )
-    parser.add_argument("--split", type=str, default=DEFAULT_SPLIT, help="Split")
+    parser.add_argument(
+        "--split", type=str, default=DEFAULT_SPLIT, help="Dataset split"
+    )
     parser.add_argument(
         "--img-url-column",
         type=str,
         default=DEFAULT_IMG_URL_COLUMN,
-        help="Image URL column",
+        help="Column name containing URL",
     )
     return parser.parse_args()
-
-
-def load_img_urls(dataset_name: str, split: str, img_url_column: str) -> pd.Series:
-    df = datasets.load_dataset(dataset_name, split=split).to_pandas()
-    return df[img_url_column]
-
-
-def select_random_img_urls(img_urls: pd.Series, n: int) -> pd.Series:
-    return img_urls.sample(n)
-
-
-def save_img_urls(img_urls: pd.Series, path: str) -> None:
-    with open(path, "w") as f:
-        f.writelines(map(lambda x: x + "\n", img_urls.tolist()))
 
 
 if __name__ == "__main__":
